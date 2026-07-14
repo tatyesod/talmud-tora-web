273a3ccf2257fcde195be459cfe29b95a16b62fb
David Ben Yair <tatyesod@gmail.com>
2026-07-14 10:41:41 +0300

    update

--- server/routes/students.js
+++ server/routes/students.js
@@ -33,7 +33,7 @@ function withDates(student) {
 }
 
 const STUDENT_SELECT = `
-  SELECT s.*, c.name AS class_name, c.parallel AS class_parallel, COALESCE(c.branch, s.branch) AS branch, co.name AS cohort_name,
+  SELECT s.*, c.name AS class_name, c.parallel AS class_parallel, COALESCE(c.branch, s.branch) AS branch, c.institution_code, co.name AS cohort_name,
          f.last_name AS family_last_name, f.father_name, f.mother_name, f.sector,
          f.home_phone, f.father_mobile, f.mother_mobile,
          f.street, f.house_number, f.apartment, f.city
